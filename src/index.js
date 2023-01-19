654a91449f88d2c34ef08a54f32f0d83af3bc8e2
Olga Gomenyuk <gomenyuk@gmail.com>
2023-01-19 23:11:38 +0200

    added forecast temperature, icons, days

--- src/index.js
+++ src/index.js
@@ -34,28 +34,38 @@ function formatDate(date) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  let forecastElement = document.querySelector('#forecast');
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
   let forecastHTML = `<div class="row">`;
-  let days = ['Thu', 'Fri', 'Sut', 'Sun', 'Mon', 'Tue'];
-  days.forEach((day) =>
-    forecastHTML = forecastHTML + `
-      <div class="col-2">
-        <div class="weather-forecast-date">
-        ${day}
-        </div>
-        <img src="#" alt = "" width="42"/>
-        <div class="weather-forecast-temperature">
-          <span class="weather-forecast-temperature-max">
-          18 °</span>
-          <span class="weather-forecast-temperature-min">
-          12 °</span>
-        </div>
-    </div>
-  `);
-  forecastHTML += `</div>`;
-  
-  forecastElement.innerHTML = forecastHTML;    
+  forecast.forEach(function (forecastDay, index) {
+    let forecastValue = `
+    <div class="col-2">
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div> 
+            <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt = "" width="42"/> 
+            <div class="weather-forecast-temperature"> 
+                <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)} °</span> 
+                <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)} °</span> 
+            </div>
+    </div>`;
+    if (index < 6) {
+      forecastHTML = forecastHTML + forecastValue;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates) {
@@ -83,19 +93,9 @@ function showWeather(response) {
   icon.setAttribute("src", 
   `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   icon.setAttribute("alt", `${response.data.weather[0].description}`);
-}
 
-function retrievePosition(position) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let units = "metric";
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
-  
-  axios.get(url).then(showWeather);
+  getForecast(response.data.coord);
 }
-
-navigator.geolocation.getCurrentPosition(retrievePosition);
 
 function searchCity(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
@@ -108,11 +108,22 @@ function handleSubmit(event) {
   let city = document.querySelector(".form-control").value;
   searchCity(city);
 }
+/*function retrievePosition(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let units = "metric";
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+  
+  axios.get(url).then(showWeather);
+}*/
 
-function getCurrentLocation(event) {
+//navigator.geolocation.getCurrentPosition(retrievePosition);
+
+/*function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
-}
+}*/
 
 function showFahrengeitTemp(event) {
   event.preventDefault();
@@ -142,7 +153,8 @@ fahrengeitLink.addEventListener("click", showFahrengeitTemp);
 let celsiumLink = document.querySelector("#celsium-link");
 celsiumLink.addEventListener("click", showCelsiumTemp);
 
-displayForecast();
+
+searchCity('Kyiv');
 
 //let currentLocationButton = document.querySelector(".current-location");
 //currentLocationButton.addEventListener("click", getCurrentLocation);
